ac0d90e22785381e64af37be0b498d47beae4e14
Антон Шмидт <schmidt.ajax@gmail.com>
2021-12-07 07:35:57 +0700

    Solution (slow)

--- solution.cpp
+++ solution.cpp
@@ -7,5 +7,15 @@
 using namespace std;
 
 void Solution::moveZeroes(vector<int> &nums) {
-  // FIXME
+  for (size_t i = 0; i < nums.size(); ++i) {
+    if (!nums[i]) {
+      size_t k = i + 1;
+      for (; k < nums.size(); ++k) {
+        if (nums[k]) break;
+      }
+      if (k < nums.size()) {
+        swap(nums[i], nums[k]);
+      } else break;
+    }
+  }
 }
